afe40fc98626cb1e3d69b391699259cd108bc8a6
suhailganaie <63506421+suhailganaie@users.noreply.github.com>
2025-09-06 12:47:59 +0530

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,10 @@
-// src/AppRoutes.jsx
 import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+// All routed pages must be default exports
 const Home = lazy(() => import('./pages/Home/Home'));
 const Earn = lazy(() => import('./pages/Earn/Earn'));
 const Tasks = lazy(() => import('./pages/Tasks/Tasks'));
-const Lottery = lazy(() => import('./pages/Lottery/Lottery'));
-const Invite = lazy(() => import('./pages/Invite/Invite'));
 
 const NotFound = () => (
   <div className="app">
@@ -22,9 +20,6 @@ export default function AppRoutes() {
         <Route index element={<Home />} />
         <Route path="/earn" element={<Earn />} />
         <Route path="/tasks" element={<Tasks />} />
-        {/* NEW routes */}
-        <Route path="/lottery" element={<Lottery />} />
-        <Route path="/invite" element={<Invite />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
