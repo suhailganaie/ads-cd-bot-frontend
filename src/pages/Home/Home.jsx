5d15f963d506c3238ba8bfe69898808568e7443b
suhailganaie <63506421+suhailganaie@users.noreply.github.com>
2025-09-06 13:54:07 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="card">
-      <h3>Home</h3>
       <p className="muted">
         Welcome to ADS BOT. Use the menu to navigate to pages like Earn or Tasks.
       </p>
